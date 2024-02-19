66f45872303ca8a59641087ecbe908105039cb05
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-02-20 02:10:23 +0530

    Add soft link in Category Card

--- src/app/nearby/CategoryCard.jsx
+++ src/app/nearby/CategoryCard.jsx
@@ -1,11 +1,12 @@
 "use client";
 import {Card, CardBody, Text, LinkBox, LinkOverlay } from "@chakra-ui/react"
+import Link from "next/link";
 
 export default function CategoryCard({category}){
     return(
         <LinkBox as={Card} bg={"brand"} align={"center"} height={"10rem"} width={"20rem"} size={"md"}>
             <CardBody className="flex flex-col items-center justify-center text-center">
-            <LinkOverlay href={`/nearby/${category}`}>
+            <LinkOverlay as={Link} href={`/nearby/${category}`}>
             <Text className="text-4xl font-semibold uppercase">{category}</Text>
             </LinkOverlay>
             </CardBody>
