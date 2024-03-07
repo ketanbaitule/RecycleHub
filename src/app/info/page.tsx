8b0f855d0ce74b8da92605542814809f4ba191fe
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-03-07 14:05:07 +0530

    Add Info Pdf

---
+++ src/app/info/page.tsx
@@ -0,0 +1,11 @@
+export default function Info(){
+    return(
+        <div className="max-w-5xl mx-auto ">
+            <div className="text-bold text-3xl my-4 py-3">Resources</div>
+            <ul className="list-disc space-y-2 text-md">
+                <li><a href="/1.pdf" target="_blank">Categorization of Waste</a></li>
+                <li><a href="/2.pdf" target="_blank">EPR 8th Amendment</a></li>
+            </ul>
+        </div>
+    )
+}
